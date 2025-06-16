5c3bebd51b6c4306d8996086c9cfe48d1164b374
Brandon <brandon@ankhstudio.com>
2025-06-16 19:39:34 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import type { AppProps } from 'next/app';  
-import { inter, lexend, radio_canada } from '@/fonts'; 
+import { inter, lexand, radio_canada } from '@/fonts'; 
 import '@/styles/globals.css'; 
 
 export default function MyApp({ Component, pageProps }: AppProps) {  
   return (  
-    <main className={`${inter.variable} ${lexend.variable} ${radio_canada.variable}`}>  
+    <main className={`${inter.variable} ${lexand.variable} ${radio_canada.variable}`}>  
       <Component {...pageProps} />  
     </main>  
   );  
