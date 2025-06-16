109f3bd31d95441d28dbdcdb079636efbdd859f2
Brandon <brandon@ankhstudio.com>
2025-06-16 19:27:24 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '@/styles/globals.scss';
-import type { AppProps } from 'next/app';
+import type { AppProps } from 'next/app';  
+import { inter, lexend, radio_canada } from '../lib/fonts'; 
+import '@/styles/globals.css'; 
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+export default function MyApp({ Component, pageProps }: AppProps) {  
+  return (  
+    <main className={`${inter.variable} ${lexend.variable} ${radio_canada.variable}`}>  
+      <Component {...pageProps} />  
+    </main>  
+  );  
+}  
+
 
 export default MyApp;
